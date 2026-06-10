f8b2c6ef7041282c4cfd2df005503b8bc849b0ce
aceaviation2026 <aceaviation2026@gmail.com>
2026-06-10 16:54:31 +0530

    Refactor database connection to use env variables
    Updated database connection to use environment variables for configuration.

--- connectiondb.js
+++ connectiondb.js
@@ -1,10 +1,30 @@
+// const mysql = require('mysql2');
+
+// const db = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'aviation_db'
+// });
+
+// db.connect((err) => {
+//     if (err) {
+//         console.log('Database Error:', err);
+//     } else {
+//         console.log('MySQL Connected');
+//     }
+// });
+
+// module.exports = db;
+
+
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'aviation_db'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.connect((err) => {
